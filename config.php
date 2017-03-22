283043b84bb4e8867fdbaca54b6dc12220cba03a
Esther <erliao@princeton.edu>
2017-03-22 00:50:05 +0000

    copied files

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'verify_token' => 'esther',
+    'token' => 'EAACEdEose0cBAMyw786dLaoB3SRG2EOCiLiDxTJIxYuDhdCFHa59RMlXsts2DHaCgdA1OXiruvjPV9sgNi8adhLfSCaC37KjUwm74YZAbaN7WXuZBE9UgHoOo7iYyxaDMbGqKVPICfkIAIXBvjGWJjaNBZAPa5dv7FInsVnN2SdnOEZCc7WWbJwhnUO3vroZD'
+];
